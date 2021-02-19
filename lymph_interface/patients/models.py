19d94c267b940c0376e023ea14bcbd3237fc9a7e
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-02-19 17:04:11 +0100

    set up a model to start with

--- lymph_interface/patients/models.py
+++ lymph_interface/patients/models.py
@@ -4,29 +4,84 @@ from django.db import models
 class Patient(models.Model):
     """Base model class of a patient. Contains patient specific information."""
     identifier = models.CharField(max_length=200) # ideally, this is a hashed id
-    gender = models.CharField(max_length=6, choices=["male". "female"])
+    gender = models.CharField(max_length=10, choices=[("female", "female"), 
+                                                      ("male"  , "male"  )])
     age = models.IntegerField()
-    
-    diagnose_date = models.DateField()
-    stage_prefix = models.CharField(max_length=1, choices=["c", "p"])
-    t_stage = models.CharField(max_length=3, choices=["Tx", "Tis", "T0", 
-                                                      "T1", "T2", "T3", "T4"])
-    n_stage = models.CharField(max_length=2, choices=["Nx", "N0", 
-                                                      "N1", "N2", "N3", "N4"])
-    m_stage = models.CharField(max_length=2, choices=["M0", "M1"])
     
     alcohol_abuse = models.BooleanField()
     nicotine_abuse = models.BooleanField()
-    hpv_status - models.BooleanField() 
+    hpv_status = models.BooleanField() 
     
     
+    def __str__(self):
+        """Report some patient specifics."""
+        return f"ID: {self.identifier}, age: {self.age}, gender: {self.gender}"
+    
+
+
+TREATMENT_TYPE = [
+    (0, "radiotherapy"),
+    (1, "chemotherapy"),
+    (2, "surgery"),
+    (3, "immunotherapy")
+]
 class Therapy(models.Model):
     """Specifies the therapy a patient has received."""
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     
-    treatment_type = models.CharField(max_length=20, choices=["radiotherapy", 
-                                                              "chemotherapy", 
-                                                              "surgery", 
-                                                              "immunotherapy"])
+    treatment_type = models.PositiveSmallIntegerField(choices=TREATMENT_TYPE)
+    
     start_date = models.DateField()
     end_date = models.DateField()
+    
+    
+    
+LOCATIONS = [
+    (0, "oral cavity"),
+    (1, "oropharynx"),
+    (2, "hypopharynx"),
+    (3, "larynx")
+]
+T_STAGES = [
+    (0, "T0"),
+    (1, "T1"),
+    (2, "T2"),
+    (3, "T3"),
+    (4, "T4")
+]
+class Tumor(models.Model):
+    """Report of primary tumor(s)."""
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    
+    diagnose_date = models.DateField()
+    
+    location = models.PositiveSmallIntegerField(choices=LOCATIONS)
+    t_stage = models.PositiveSmallIntegerField(choices=T_STAGES)
+    stage_prefix = models.CharField(max_length=1, choices=[("c", "c"), 
+                                                           ("p", "p")])
+    
+    
+N_STAGES = [
+    (0, "N0"),
+    (1, "N1"),
+    (2, "N2"),
+    (3, "N3")
+]
+M_STAGES = [
+    (0, "M0"),
+    (1, "M1")
+]
+class Diagnose(models.Model):
+    """Report of pattern of lymphatic metastases for a given diagnostic 
+    modality."""
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    
+    diagnose_date = models.DateField()
+    
+    n_stage = models.PositiveSmallIntegerField(choices=N_STAGES)
+    m_stage = models.PositiveSmallIntegerField(choices=M_STAGES)
+    
+    lnl_I   = models.BooleanField(blank=True, null=True, default=None)
+    lnl_II  = models.BooleanField(blank=True, null=True, default=None)
+    lnl_III = models.BooleanField(blank=True, null=True, default=None)
+    lnl_IV  = models.BooleanField(blank=True, null=True, default=None)
