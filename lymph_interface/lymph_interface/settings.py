e1e6d3d0eb0a240eb3976c2f42032d493d2da2e2
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-02-22 23:17:38 +0100

    added base header & footer

--- lymph_interface/lymph_interface/settings.py
+++ lymph_interface/lymph_interface/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 STATIC_URL = '/static/'
