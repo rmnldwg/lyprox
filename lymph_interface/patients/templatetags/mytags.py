990856b3ad4721234486ac44e5ab6f678efb1bbe
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-04-20 17:18:51 +0200

    made pretty barplots for involvement

--- lymph_interface/patients/templatetags/mytags.py
+++ lymph_interface/patients/templatetags/mytags.py
@@ -4,3 +4,11 @@ register = template.Library()
 @register.filter(name="index")
 def index(indexable, i):
     return indexable[i]
+
+@register.filter(name="bar")
+def bar(indexable, argstr):
+    istr, widthstr = argstr.split(',')
+    i = int(istr)
+    width = float(widthstr)
+    total = sum(indexable)
+    return width * indexable[i] / total
