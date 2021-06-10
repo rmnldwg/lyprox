a285b3a9ce3e5be46576df9c756b2c7cb9b9db02
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-06-10 19:14:47 +0200

    added authentication system. login works

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
