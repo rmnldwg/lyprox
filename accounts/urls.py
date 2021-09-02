654b4168c6fef8f10084c1430f88cc6c5d7b9872
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2021-09-02 22:39:41 +0200

    made signup form & html, save/send still missing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,23 @@
+from accounts.views import signup_request_view
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
 
-from . import forms
+from .forms import CustomAuthenticationForm
+from .views import signup_request_view
 
 app_name = "accounts"
+
+login_view = auth_views.LoginView.as_view(
+    template_name="accounts/login.html",
+    authentication_form=CustomAuthenticationForm
+)
+logout_view = auth_views.LogoutView.as_view(
+    template_name="accounts/logout.html"
+)
+
 urlpatterns = [
-    path('login/',
-         auth_views.LoginView.as_view(
-             template_name="accounts/login.html",
-             authentication_form=forms.CustomAuthenticationForm
-         ),
-         name='login'),
-    path('logout/',
-         auth_views.LogoutView.as_view(template_name="accounts/logout.html"),
-         name='logout'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup_request_view, name='signup_request')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,21 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.views import generic
 
-from .forms import UserForm, InstitutionForm
-from .models import Institution
+from .forms import SignupRequestForm
+
+
+def signup_request_view(request):
+    """Render form for requesting to be signed up as a user."""
+    if request.method == "POST":
+        form = SignupRequestForm(request.POST)
+        
+        if form.is_valid():
+            # TODO: Implement some sort of notification to notify the admin(s) 
+            #   that a new request has been made and store the request data.
+            pass
+    
+    else:
+        form = SignupRequestForm()
+        
+    context = {"form": form}
+    return render(request, "accounts/signup_request.html", context)
