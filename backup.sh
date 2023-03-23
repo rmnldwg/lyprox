4bc7e3892c1f84ad61c3d800dd409a1d7fc0a543
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2023-03-23 11:47:57 +0100

    fix: make backup script create correct directory

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-dir_name=${PWD##*/}
+dir_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+dir_name=$( basename $dir_path )
 backup_dir=/var/local/backups/$dir_name
-backup_name=$(date +"%Y-%m-%d.bak")
+backup_name=$( date +"%Y-%m-%d.bak" )
 
 mkdir -p $backup_dir
 sqlite3 db.sqlite3 ".backup '$backup_dir/$backup_name'"
