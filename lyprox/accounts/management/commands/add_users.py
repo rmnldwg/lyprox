f2114adcc8e9fde0920aad50962d49285d52845c
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2023-11-28 11:33:55 +0100

    change(accounts): allow add users flexibly
    Same as for institutions and risk models.
    
    Related: #109

---
+++ lyprox/accounts/management/commands/add_users.py
@@ -0,0 +1,107 @@
+"""Command to initialize users."""
+import json
+import os
+from pathlib import Path
+
+from django.contrib.auth.hashers import make_password
+from django.core.management import base
+from django.db.utils import IntegrityError
+
+from lyprox.accounts.models import Institution, User
+
+
+class Command(base.BaseCommand):
+    """Command to initialize users."""
+    help = __doc__
+
+    def add_arguments(self, parser):
+        """Add arguments to command."""
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument(
+            "--from-file",
+            type=Path,
+            help="Path to JSON file with list of users.",
+        )
+        group.add_argument(
+            "--from-stdin",
+            action="store_true",
+            help="Use command line arguments to create a single user.",
+        )
+        parser.add_argument(
+            "--email", type=str, help="Email of user.",
+        )
+        parser.add_argument(
+            "--first-name", type=str, help="First name of user.",
+        )
+        parser.add_argument(
+            "--last-name", type=str, help="Last name of user.",
+        )
+        parser.add_argument(
+            "--institution", type=str, help="Abbreviation of institution.",
+        )
+        parser.add_argument(
+            "--is-active", action="store_true", default=True,
+            help="Is user active?",
+        )
+        parser.add_argument(
+            "--is-staff", action="store_true", default=False,
+            help="Is user staff?",
+        )
+        parser.add_argument(
+            "--is-superuser", action="store_true", default=False,
+            help="Is user superuser?",
+        )
+
+    def handle(self, *args, **options):
+        """Execute command."""
+        if not options["from_stdin"]:
+            with open(options["from_file"], "r", encoding="utf-8") as json_file:
+                user_configurations = json.load(json_file)
+        else:
+            user_configurations = [{
+                "email": options["email"],
+                "first_name": options["first_name"],
+                "last_name": options["last_name"],
+                "institution": options["institution"],
+                "is_active": options["is_active"],
+                "is_staff": options["is_staff"],
+                "is_superuser": options["is_superuser"],
+            }]
+
+
+        for config in user_configurations:
+            env_name = (
+                "DJANGO_"
+                + config['email'].split('@')[0].upper().replace('.', '')
+                + "_PASSWORD"
+            )
+
+            try:
+                env_value = os.environ[env_name]
+            except KeyError:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"Environment variable '{env_name}' not found. Skipping."
+                    )
+                )
+                continue
+
+            try:
+                config["password"] = make_password(env_value)
+                config["institution"] = Institution.objects.get(shortname=config['institution'])
+                User.objects.create(**config)
+                self.stdout.write(
+                    self.style.SUCCESS(f"User '{config['email']}' created.")
+                )
+            except IntegrityError:
+                self.stdout.write(
+                    self.style.WARNING(
+                        f"User '{config['email']}' already exists. Skipping."
+                    )
+                )
+            except Exception as exc:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"User '{config['email']}' could not be created: {exc}"
+                    )
+                )
