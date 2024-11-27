948a01f4b60cb48cfe0a1456b36234983c58b3c6
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-11-27 18:14:34 +0100

    feat: add subsite labels

--- lyprox/settings.py
+++ lyprox/settings.py
@@ -20,6 +20,7 @@ Only these env vars should need to be changed:
 
 import os
 from pathlib import Path
+from typing import Literal
 
 from django import urls
 from django.db import models
@@ -81,114 +82,112 @@ class TStages(models.IntegerChoices):
 # the subsites. Then I can use the labels in the dashboard.
 class BaseOfTongueSubsites(models.TextChoices):
     """Tumor subsites in the base of the tongue."""
-    C01   = "C01"  , "C01"
-    C01_9 = "C01.9", "C01.9"  # technically invalid ICD-10 code, but often used
-
-class TonsilSubsites(models.TextChoices):
-    """Tumor subsites in the tonsils."""
-    C09   = "C09"  , "C09"
-    C09_0 = "C09.0", "C09.0"
-    C09_1 = "C09.1", "C09.1"
-    C09_8 = "C09.8", "C09.8"
-    C09_9 = "C09.9", "C09.9"
-
-class RestOropharynxSubsites(models.TextChoices):
-    """Tumor subsites in the rest of the oropharynx."""
-    C10   = "C10"  , "C10"
-    C10_0 = "C10.0", "C10.0"
-    C10_1 = "C10.1", "C10.1"
-    C10_2 = "C10.2", "C10.2"
-    C10_3 = "C10.3", "C10.3"
-    C10_4 = "C10.4", "C10.4"
-    C10_8 = "C10.8", "C10.8"
-    C10_9 = "C10.9", "C10.9"
-
-class RestHypopharynxSubsites(models.TextChoices):
-    """Tumor subsites in the rest of the hypopharynx."""
-    C12   = "C12"  , "C12"
-    C12_9 = "C12.9", "C12.9"
-    C13   = "C13"  , "C13"
-    C13_0 = "C13.0", "C13.0"
-    C13_1 = "C13.1", "C13.1"
-    C13_2 = "C13.2", "C13.2"
-    C13_8 = "C13.8", "C13.8"
-    C13_9 = "C13.9", "C13.9"
-
-class GlottisSubsites(models.TextChoices):
-    """Tumor subsites in the glottis."""
-    C32_0 = "C32.0", "C32.0"
-
-class SupraglottisSubsites(models.TextChoices):
-    """Tumor subsites in the supraglottis."""
-    C32_1 = "C32.1", "C32.1"
-
-class SubglottisSubsites(models.TextChoices):
-    """Tumor subsites in the subglottis."""
-    C32_2 = "C32.2", "C32.2"
-
-class RestLarynxSubsites(models.TextChoices):
-    """Tumor subsites in the rest of the larynx."""
-    C32   = "C32"  , "C32"
-    C32_1 = "C32.1", "C32.1"
-    C32_2 = "C32.2", "C32.2"
-    C32_3 = "C32.3", "C32.3"
-    C32_8 = "C32.8", "C32.8"
-    C32_9 = "C32.9", "C32.9"
+    C01   = "C01"  , "Base of Tongue"
+    C01_9 = "C01.9", "Base of Tongue"  # technically invalid ICD-10 code, but often used
 
 class TongueSubsites(models.TextChoices):
     """Tumor subsites in the tongue."""
-    C02   = "C02"  , "C02"
-    C02_0 = "C02.0", "C02.0"
-    C02_1 = "C02.1", "C02.1"
-    C02_2 = "C02.2", "C02.2"
-    C02_3 = "C02.3", "C02.3"
-    C02_4 = "C02.4", "C02.4"
-    C02_8 = "C02.8", "C02.8"
-    C02_9 = "C02.9", "C02.9"
-
-class GumsAndCheeksSubsites(models.TextChoices):
-    """Tumor subsites in the gums and cheeks."""
-    C03   = "C03"  , "C03"
-    C03_0 = "C03.0", "C03.0"
-    C03_1 = "C03.1", "C03.1"
-    C03_9 = "C03.9", "C03.9"
-    C06   = "C06"  , "C06"
-    C06_0 = "C06.0", "C06.0"
-    C06_1 = "C06.1", "C06.1"
-    C06_2 = "C06.2", "C06.2"
-    C06_8 = "C06.8", "C06.8"
-    C06_9 = "C06.9", "C06.9"
+    C02   = "C02"  , "Tongue"
+    C02_0 = "C02.0", "Dorsal surface of tongue"
+    C02_1 = "C02.1", "Border of tongue"
+    C02_2 = "C02.2", "Ventral surface of tongue"
+    C02_3 = "C02.3", "Anterior two-thirds of tongue"
+    C02_4 = "C02.4", "Lingual tonsil"
+    C02_8 = "C02.8", "Overlapping lesion of tongue"
+    C02_9 = "C02.9", "Tongue, unspecified"
 
 class FloorOfMouthSubsites(models.TextChoices):
     """Tumor subsites in the floor of the mouth."""
-    C04   = "C04"  , "C04"
-    C04_0 = "C04.0", "C04.0"
-    C04_1 = "C04.1", "C04.1"
-    C04_8 = "C04.8", "C04.8"
-    C04_9 = "C04.9", "C04.9"
+    C04   = "C04"  , "Floor of Mouth"
+    C04_0 = "C04.0", "Anterior floor of mouth"
+    C04_1 = "C04.1", "Lateral floor of mouth"
+    C04_8 = "C04.8", "Overlapping lesion of floor of mouth"
+    C04_9 = "C04.9", "Floor of mouth, unspecified"
 
 class PalateSubsites(models.TextChoices):
     """Tumor subsites in the palate."""
-    C05   = "C05"  , "C05"
-    C05_0 = "C05.0", "C05.0"
-    C05_1 = "C05.1", "C05.1"
-    C05_2 = "C05.2", "C05.2"
-    C05_8 = "C05.8", "C05.8"
-    C05_9 = "C05.9", "C05.9"
+    C05   = "C05"  , "Palate"
+    C05_0 = "C05.0", "Hard palate"
+    C05_1 = "C05.1", "Soft palate"
+    C05_2 = "C05.2", "Uvula"
+    C05_8 = "C05.8", "Overlapping lesion of palate"
+    C05_9 = "C05.9", "Palate, unspecified"
+
+class GumsAndCheeksSubsites(models.TextChoices):
+    """Tumor subsites in the gums and cheeks."""
+    C03   = "C03"  , "Gum"
+    C03_0 = "C03.0", "Upper gum"
+    C03_1 = "C03.1", "Lower gum"
+    C03_9 = "C03.9", "Gum, unspecified"
+    C06   = "C06"  , "Other and unspecified parts of mouth"
+    C06_0 = "C06.0", "Cheek mucosa"
+    C06_1 = "C06.1", "Vestibule of mouth"
+    C06_2 = "C06.2", "Retromolar area"
+    C06_8 = "C06.8", "Overlapping lesion of other and unspecified parts of mouth"
+    C06_9 = "C06.9", "Mouth, unspecified"
 
 class GlandsSubsites(models.TextChoices):
     """Tumor subsites in the glands."""
-    C08   = "C08"  , "C08"
-    C08_0 = "C08.0", "C08.0"
-    C08_1 = "C08.1", "C08.1"
-    C08_9 = "C08.9", "C08.9"
+    C08   = "C08"  , "Salivary glands"
+    C08_0 = "C08.0", "Submandibular gland"
+    C08_1 = "C08.1", "Sublingual gland"
+    C08_9 = "C08.9", "Major salivary gland, unspecified"
+
+class TonsilSubsites(models.TextChoices):
+    """Tumor subsites in the tonsils."""
+    C09   = "C09"  , "Tonsil"
+    C09_0 = "C09.0", "Tonsillar fossa"
+    C09_1 = "C09.1", "Tonsillar pillar (anterior)(posterior)"
+    C09_8 = "C09.8", "Overlapping lesion of tonsil"
+    C09_9 = "C09.9", "Tonsil, unspecified"
+
+class RestOropharynxSubsites(models.TextChoices):
+    """Tumor subsites in the rest of the oropharynx."""
+    C10   = "C10"  , "Oropharynx"
+    C10_0 = "C10.0", "Vallecula"
+    C10_1 = "C10.1", "Anterior surface of epiglottis"
+    C10_2 = "C10.2", "Lateral wall of oropharynx"
+    C10_3 = "C10.3", "Posterior wall of oropharynx"
+    C10_4 = "C10.4", "Branchial cleft"
+    C10_8 = "C10.8", "Overlapping lesion of oropharynx"
+    C10_9 = "C10.9", "Oropharynx, unspecified"
+
+class RestHypopharynxSubsites(models.TextChoices):
+    """Tumor subsites in the rest of the hypopharynx."""
+    C12   = "C12"  , "Piriform sinus"
+    C12_9 = "C12.9", "Piriform sinus"  # technically invalid ICD-10 code, but often used
+    C13   = "C13"  , "Hypopharynx"
+    C13_0 = "C13.0", "Postcricoid region"
+    C13_1 = "C13.1", "Aryepiglottic fold, hypopharyngeal aspect"
+    C13_2 = "C13.2", "Posterior wall of hypopharynx"
+    C13_8 = "C13.8", "Overlapping lesion of hypopharynx"
+    C13_9 = "C13.9", "Hypopharynx, unspecified"
+
+class GlottisSubsites(models.TextChoices):
+    """Tumor subsites in the glottis."""
+    C32_0 = "C32.0", "Glottis"
+
+class SupraglottisSubsites(models.TextChoices):
+    """Tumor subsites in the supraglottis."""
+    C32_1 = "C32.1", "Supraglottis"
+
+class SubglottisSubsites(models.TextChoices):
+    """Tumor subsites in the subglottis."""
+    C32_2 = "C32.2", "Subglottis"
+
+class RestLarynxSubsites(models.TextChoices):
+    """Tumor subsites in the rest of the larynx."""
+    C32   = "C32"  , "Larynx"
+    C32_3 = "C32.3", "Laryngeal cartilage"
+    C32_8 = "C32.8", "Overlapping lesion of larynx"
+    C32_9 = "C32.9", "Larynx, unspecified"
 
 
-SUBSITE_DICT = {
+SUBSITE_CHOICES_DICT = {
     "Base of Tongue": BaseOfTongueSubsites.choices,
     "Tonsil": TonsilSubsites.choices,
     "Rest Oropharynx": RestOropharynxSubsites.choices,
-    "Rest Hypopharynx": RestHypopharynxSubsites.choices,
+    "Hypopharynx": RestHypopharynxSubsites.choices,
     "Glottis": GlottisSubsites.choices,
     "Supraglottis": SupraglottisSubsites.choices,
     "Subglottis": SubglottisSubsites,
@@ -199,20 +198,30 @@ SUBSITE_DICT = {
     "Palate": PalateSubsites.choices,
     "Glands": GlandsSubsites.choices,
 }
-SUBSITE_LIST = [
-    choice
-    for choices_list in SUBSITE_DICT.values()
-    for choice in choices_list
-]
 
-def get_choices_for(subsites: list[str]) -> list[tuple[str, str]]:
-    """Return the choices for the given subsites."""
+def get_subsite_choices_for(
+    subsites: list[str] | Literal["all"] = "all",
+) -> list[tuple[str, str]]:
+    """Return the choice tuples for the given subsites."""
+    subsites = SUBSITE_CHOICES_DICT.keys() if subsites == "all" else subsites
     return [
         choice
         for subsite in subsites
-        for choice in SUBSITE_DICT[subsite].choices
+        for choice in SUBSITE_CHOICES_DICT[subsite]
     ]
 
+def get_subsite_values_for(
+    subsites: list[str] | Literal["all"] = "all",
+) -> list[str]:
+    """Return the choice values for the given subsites."""
+    subsites = SUBSITE_CHOICES_DICT.keys() if subsites == "all" else subsites
+    return [
+        choice[0]
+        for subsite in subsites
+        for choice in SUBSITE_CHOICES_DICT[subsite]
+    ]
+
+SUBSITE_CHOICES_LIST = get_subsite_choices_for("all")
 
 CSRF_COOKIE_SECURE = not DEBUG
 CRSF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
