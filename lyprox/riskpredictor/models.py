81a17e877a70cf494183851869be532c81a355fb
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-02-05 17:37:03 +0100

    change: finalize new `InferenceResult` db model

--- lyprox/riskpredictor/models.py
+++ lyprox/riskpredictor/models.py
@@ -19,16 +19,14 @@ import numpy as np
 import yaml
 from django.db import models
 from dvc.api import DVCFileSystem
-from github import Github, Repository
 from joblib import Memory
-from lydata.loader import _get_github_auth
 from lymph.types import Model
 from lyscripts.configs import (
     DeprecatedModelConfig,
     DistributionConfig,
     GraphConfig,
     ModelConfig,
-    add_dists,
+    add_distributions,
     construct_model,
 )
 from pydantic import TypeAdapter
@@ -39,17 +37,18 @@ logger = logging.getLogger(__name__)
 memory = Memory(location=settings.JOBLIB_CACHE_DIR, verbose=0)
 
 
-def fetch_and_merge_yaml(
+@memory.cache
+def cached_fetch_and_merge_yaml(
     repo_name: str,
     ref: str,
     graph_config_path: str,
     model_config_path: str,
-    distributions_config_path: str,
+    dist_configs_path: str,
 ) -> dict[str, Any]:
     """Fetch and merge the YAML configuration files from the repo."""
     repo_url = f"https://github.com/{repo_name}"
     dvc_fs = DVCFileSystem(url=repo_url, rev=ref)
-    config_paths = [graph_config_path, model_config_path, distributions_config_path]
+    config_paths = [graph_config_path, model_config_path, dist_configs_path]
     merged_config = {}
     for config_path in config_paths:
         with dvc_fs.open(config_path) as config_file:
@@ -59,34 +58,37 @@ def fetch_and_merge_yaml(
     return merged_config
 
 
-def validate_configs(
-    merged_yaml: dict[str, Any],
-) -> tuple[GraphConfig, ModelConfig, dict[str | int, DistributionConfig]]:
+ConfigTupleType = tuple[GraphConfig, ModelConfig, dict[str | int, DistributionConfig]]
+
+
+def validate_configs(merged_yaml: dict[str, Any]) -> ConfigTupleType:
     """Validate the pydantic configs necessary for constructing the model."""
     graph_config = GraphConfig.model_validate(merged_yaml["graph"])
 
     if merged_yaml.get("version") != 1:
         deprecated = DeprecatedModelConfig.model_validate(merged_yaml["model"])
-        model_config, distributions_config = deprecated.translate()
-        return graph_config, model_config, distributions_config
+        model_config, dist_configs = deprecated.translate()
+        return graph_config, model_config, dist_configs
 
     model_config = ModelConfig.model_validate(merged_yaml["model"])
     adapter = TypeAdapter(dict[str | int, DistributionConfig])
-    distributions_config = adapter.validate_python(merged_yaml["distributions"])
-    return graph_config, model_config, distributions_config
+    dist_configs = adapter.validate_python(merged_yaml["distributions"])
+    return graph_config, model_config, dist_configs
 
 
-def construct_model_and_add_dists(
+@memory.cache
+def cached_construct_model_and_add_dists(
     graph_config: GraphConfig,
     model_config: ModelConfig,
-    distributions_config: dict[str | int, DistributionConfig],
+    dist_configs: dict[str | int, DistributionConfig],
 ) -> Model:
     """Construct the lymph model and add the distributions to it."""
     model = construct_model(model_config=model_config, graph_config=graph_config)
-    return add_dists(model=model, distributions=distributions_config)
+    return add_distributions(model=model, configs=dist_configs)
 
 
-def fetch_model_samples(
+@memory.cache
+def cached_fetch_model_samples(
     repo_name: str,
     ref: str,
     samples_path: str,
@@ -114,7 +116,8 @@ def fetch_model_samples(
     return samples[rand_idx]
 
 
-def compute_priors(
+@memory.cache
+def cached_compute_priors(
     model: Model,
     samples: np.ndarray,
     t_stage: str | int,
@@ -123,7 +126,7 @@ def compute_priors(
     priors = []
 
     for sample in samples:
-        model.set_params(*sample)
+        model.set_named_params(*sample)
         priors.append(model.state_dist(t_stage=t_stage))
 
     return np.stack(priors)
@@ -134,7 +137,7 @@ class InferenceResult(loggers.ModelLoggerMixin, models.Model):
     Results of an inference run of the ``lymph-model`` package.
 
     It fetches the HDF5 parameter samples from the DVC remote storage and uses them to
-    compute the prior risk matrices and stores them in an HDF5 file, too.
+    compute the prior risk matrices and caches them using joblib.
     """
 
     repo_name = models.CharField(max_length=50, default="rmnldwg/lynference")
@@ -145,7 +148,7 @@ class InferenceResult(loggers.ModelLoggerMixin, models.Model):
     """Path to YAML file containing the graph configuration inside the git repo."""
     model_config_path = models.CharField(max_length=100, default="model.ly.yaml")
     """Path to YAML file containing the model configuration inside the git repo."""
-    distributions_config_path = models.CharField(
+    dist_configs_path = models.CharField(
         max_length=100,
         default="distributions.ly.yaml",
     )
@@ -158,52 +161,65 @@ class InferenceResult(loggers.ModelLoggerMixin, models.Model):
     class Meta:
         """Meta options for the `InferenceResult` model."""
 
-        unique_together = ("repo_name", "ref")
+        unique_together = (
+            "repo_name",
+            "ref",
+            "graph_config_path",
+            "model_config_path",
+            "dist_configs_path",
+            "samples_path",
+        )
 
-    def get_repo(
-        self,
-        token: str | None = None,
-        user: str | None = None,
-        password: str | None = None,
-    ) -> Repository:
-        """Return the GitHub repository object."""
-        auth = _get_github_auth(token=token, user=user, password=password)
-        gh = Github(auth=auth)
-        repo = gh.get_repo(self.repo_name)
-        logger.info(f"Fetched GitHub repository {self.repo_name}")
-        return repo
+    def __str__(self) -> str:
+        """Return the string representation of the instance."""
+        return f"{self.repo_name}@{self.ref}"
 
     def get_merged_yaml(self) -> dict[str, Any]:
         """Fetch and merge the YAML configuration files from the repo."""
-        return memory.cache(fetch_and_merge_yaml)(
+        return cached_fetch_and_merge_yaml(
             repo_name=self.repo_name,
             ref=self.ref,
             graph_config_path=self.graph_config_path,
             model_config_path=self.model_config_path,
-            distributions_config_path=self.distributions_config_path,
+            dist_configs_path=self.dist_configs_path,
         )
 
-    def validate_configs(
-        self,
-    ) -> tuple[GraphConfig, ModelConfig, dict[str | int, DistributionConfig]]:
+    def validate_configs(self) -> ConfigTupleType:
         """Validate the pydantic configs necessary for constructing the model."""
         merged_yaml = self.get_merged_yaml()
         return validate_configs(merged_yaml)
 
     def construct_model(self) -> Model:
         """Create the lymph model instance from the validated configs."""
-        graph_config, model_config, distributions_config = self.validate_configs()
-        return memory.cache(construct_model_and_add_dists)(
+        graph_config, model_config, dist_configs = self.validate_configs()
+        return cached_construct_model_and_add_dists(
             graph_config=graph_config,
             model_config=model_config,
-            distributions_config=distributions_config,
+            dist_configs=dist_configs,
         )
 
     def fetch_samples(self) -> np.ndarray:
         """Fetch the model samples from the HDF5 file in the DVC repo."""
-        return memory.cache(fetch_model_samples)(
+        return cached_fetch_model_samples(
             repo_name=self.repo_name,
             ref=self.ref,
             samples_path=self.samples_path,
             num_samples=self.num_samples,
         )
+
+    def compute_priors(self, t_stage: int | str) -> np.ndarray:
+        """Compute priors for every T-stage using the model samples."""
+        return cached_compute_priors(
+            model=self.construct_model(),
+            samples=self.fetch_samples(),
+            t_stage=t_stage,
+        )
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        """Save the instance and fill the cache with precomputed priors."""
+        for t_stage in self.construct_model().get_all_distributions():
+            priors = self.compute_priors(t_stage=t_stage)
+            self.logger.info(
+                f"{self} precomputed prior for {t_stage=} with {priors.shape=}."
+            )
+        return super().save(*args, **kwargs)
